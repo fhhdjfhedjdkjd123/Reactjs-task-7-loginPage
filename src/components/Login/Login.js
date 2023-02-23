c3e647c930d3a8d8d5dd7b953aabf3e44670642f
fhhdjfhedjdkjd123 <bash>
2023-02-23 20:32:22 +0530

    .

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,6 +6,7 @@ import Button from '../UI/Button/Button';
 
 const emailReducer=(state,action)=>{
   if(action.type === "USER_EMAIL_INPUT"){
+    
     return {value:action.val, isValid:action.val.includes("@")}
   }
   if(action.type === "INPUT_BLUR"){
